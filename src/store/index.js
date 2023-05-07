f4c7ea0a229bcf2813f209bb59a915db502058ef
xunlu129 <3181716884@qq.com>
2023-05-08 02:22:11 +0800

    feat: 完成了侧边栏设计

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,23 @@ export default createStore({
     state: {
         // 是否登录
         isLogin: false,
+        // 用户创建的歌单
+        createdMusicList: [],
+        // 已收藏的歌单
+        collectMusicList: [],
     },
     mutations: {
         //更新登录状态
         updateLoginState(state, flag = false) {
             state.isLogin = flag;
+        },
+        // 更新用户创建的歌单
+        updateCreatedMusicList(state, createdMusicList) {
+            state.createdMusicList = createdMusicList;
+        },
+        // 更新已收藏的歌单
+        updateCollectMusicList(state, collectMusicList) {
+            state.collectMusicList = collectMusicList;
         },
     }
 })
--- src/views/IndexVue.vue
+++ src/views/IndexVue.vue
@@ -3,6 +3,51 @@
         <el-header>
             <HeaderBar></HeaderBar>
         </el-header>
+        <el-container>
+            <!--侧边栏-->
+            <el-aside width="220px">
+                <el-menu active-text-color="black"
+                         text-color="rgb(75, 75, 75)"
+                         router :default-active="defaultActive">
+                    <el-menu-item index="/index">
+                        <i class="iconfont icon-yinle"></i>
+                        <span>发现音乐</span>
+                    </el-menu-item>
+                    <el-menu-item index="/video">
+                        <i class="iconfont icon-shipin"></i>
+                        <span>视频</span>
+                    </el-menu-item>
+                    <el-menu-item index="/favorites">
+                        <i class="iconfont icon-more"></i>
+                        <span>收藏</span>
+                    </el-menu-item>
+                    <el-menu-item index="/recommendmusic">
+                        <i class="iconfont icon-good"></i>
+                        <span>每日推荐</span>
+                    </el-menu-item>
+                    <el-menu-item-group v-if="createdMusicList.length != 0" title="创建的歌单">
+                        <el-menu-item v-for="(item, index) in createdMusicList"
+                                      :key="index"
+                                      :index="'/musiclistdetail/' + item.id">
+                            <i class="iconfont icon-gedan"></i>
+                            <span>{{ item.name }}</span>
+                        </el-menu-item>
+                    </el-menu-item-group>
+                    <el-menu-item-group v-if="collectedMusicList.length != 0" title="收藏的歌单">
+                        <el-menu-item v-for="(item, index) in collectedMusicList"
+                                      :key="index"
+                                      :index="'/musiclistdetail/' + item.id">
+                            <i class="iconfont icon-gedan"></i>
+                            <span>{{ item.name }}</span>
+                        </el-menu-item>
+                    </el-menu-item-group>
+                </el-menu>
+            </el-aside>
+            <!-- 主体内容 -->
+            <el-main>
+
+            </el-main>
+        </el-container>
     </el-container>
 </template>
 
@@ -12,7 +57,75 @@ import HeaderBar from "components/headerBar/HeaderBar.vue"
 export default {
     components: {
         HeaderBar,
-    }
+    },
+    data() {
+        return {
+            defaultActive: '',
+            // 创建的歌单
+            createdMusicList: [],
+            // 收藏的歌单
+            collectedMusicList: [],
+        }
+    },
+    created() {
+        if (this.$route.path.search("/musiclistdetail") == -1) {
+            this.defaultActive = "/" + this.$route.path.split("/")[1];
+        } else {
+            this.defaultActive = this.$route.path;
+        }
+    },
+    methods: {
+        // 请求
+        // 请求用户歌单
+        async getUserMusicList() {
+            if (!this.$store.state.isLogin) {
+                // 说明已经退出登录
+                this.$message.error("请先进行登录操作");
+                return;
+            }
+            let timestamp = Date.parse(new Date());
+            let res = await this.$request("/user/playlist", {
+                uid: window.localStorage.getItem("userId"),
+                timestamp,
+            });
+            console.log(res);
+            // 对数据进行处理分类
+            res = res.data.playlist;
+            let index = res.findIndex((item) => item.subscribed == true);
+            this.createdMusicList = res.slice(0, index);
+            this.collectedMusicList = res.slice(index);
+            this.createdMusicList[0].name = "我喜欢的音乐";
+            // console.log(this.createdMusicList, this.collectedMusicList);
+            // 将创建的歌单上传至vuex
+            this.$store.commit("updateCreatedMusicList", this.createdMusicList);
+            // 将收藏的歌单上传至vuex
+            this.$store.commit("updateCollectMusicList", this.collectedMusicList);
+        },
+    },
+    watch: {
+        // 监听收藏歌单的变化
+        "$store.state.collectMusicList"(current) {
+            this.collectedMusicList = current;
+        },
+        "$route.path"(current) {
+            // 取路由中的首地址 用于侧边栏的导航active
+            if (current.search("/musiclistdetail") == -1) {
+                this.defaultActive = "/" + current.split("/")[1];
+            } else {
+                this.defaultActive = current;
+            }
+        },
+        // 监听vuex中的登录状态
+        "$store.state.isLogin"(current) {
+            if (current) {
+                this.getUserMusicList();
+            } else {
+                // 清空收藏和创建歌单
+                this.createdMusicList = [];
+                this.collectedMusicList = [];
+            }
+        },
+    },
 }
 </script>
 
@@ -25,4 +138,11 @@ export default {
   margin: 0;
   z-index: 100;
 }
+
+.iconfont {
+  font-size: 18px;
+  margin: 5px !important;
+  color: rgb(150, 150, 150);
+}
+
 </style>
